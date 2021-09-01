23c8208a0268c732ea13634e284737645a84e451
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-09-01 11:53:42 -0300

    Added root endpoint redirection

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PortalController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PortalController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.servlet.view.RedirectView;
 import java.util.List;
 
 @Controller
@@ -14,6 +14,11 @@ public class PortalController {
 
     @Autowired
     CourseService courseService;
+
+    @RequestMapping("/")
+    public RedirectView rootRedirect() {
+        return new RedirectView("portal");
+    }
 
     @RequestMapping("/portal")
     public ModelAndView portal() {
