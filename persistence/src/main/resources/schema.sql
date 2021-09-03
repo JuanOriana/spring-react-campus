1c5d7e973b46e755c9f027bdf0140794530083cf
santigarciam <sangarcia@itba.edu.ar>
2021-09-02 21:42:22 -0300

    Updated courses table. SubjectId is autoincremetal

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -7,18 +7,19 @@ CREATE TABLE IF NOT EXISTS  teachers (
                 password varchar (50) );
 
 CREATE TABLE IF NOT EXISTS  courses (
-                subjectId INTEGER PRIMARY KEY,
+                subjectId SERIAL PRIMARY KEY,
                 name varchar (50),
                 code varchar(50),
                 quarter INTEGER ,
                 board varchar(50),
-                year INTEGER );
+                year INTEGER,
+                UNIQUE(code,quarter,board,year));
 
         CREATE TABLE IF NOT EXISTS  announcement (
                 announcementId INTEGER,
                 teacherId INTEGER,
                 subjectId INTEGER, 
                 title varchar (50), 
-                content varchar, 
+                content TEXT ,
                 date DATE, 
                 PRIMARY KEY(announcementId, teacherId, subjectId) );
