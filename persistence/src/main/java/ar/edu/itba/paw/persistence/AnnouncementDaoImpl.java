581cc314b91ef8b9d41deb71be225cab0f13d628
santigarciam <sangarcia@itba.edu.ar>
2021-09-03 12:56:05 -0300

    Modified row mapper lambda function and removed courseId when inserting

--- persistence/src/main/java/ar/edu/itba/paw/persistence/AnnouncementDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/AnnouncementDaoImpl.java
@@ -2,7 +2,6 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.interfaces.AnnouncementDao;
 import ar.edu.itba.paw.models.Announcement;
-import ar.edu.itba.paw.models.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -17,7 +16,11 @@ public class AnnouncementDaoImpl implements AnnouncementDao {
 
     private JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert jdbcInsert;
-    private static  final RowMapper<Announcement> ROW_MAPPER = (rs, rowNum) -> new Announcement(rs.getLong("announcementId"),rs.getLong("teacherId"),rs.getLong("subjectId"),rs.getDate("date"),rs.getString("title"),rs.getString("content"));
+    private static final RowMapper<Announcement> ROW_MAPPER = (rs, rowNum) -> {
+        Announcement announcement = new Announcement(rs.getLong("teacherId"), rs.getLong("courseId"), rs.getDate("date"), rs.getString("title"), rs.getString("content"));
+        announcement.setAnnouncementId(rs.getLong("announcementId"));
+        return announcement;
+    };
 
     @Autowired
     public AnnouncementDaoImpl(final DataSource ds) {
@@ -27,28 +30,26 @@ public class AnnouncementDaoImpl implements AnnouncementDao {
 
     @Override
     public boolean create(Announcement announcement) {
-        final Map<String,Object> args = new HashMap<>();
-        args.put("announcementId",announcement.getAnnouncementId());
-        args.put("subjectId",announcement.getSubjectId());
-        args.put("date",announcement.getDate());
-        args.put("title",announcement.getTitle());
-        args.put("content",announcement.getContent());
-        args.put("teacherId",announcement.getTeacherId());
+        final Map<String, Object> args = new HashMap<>();
+        args.put("date", announcement.getDate());
+        args.put("title", announcement.getTitle());
+        args.put("content", announcement.getContent());
+        args.put("teacherId", announcement.getTeacherId());
 
         final Number rowsAffected = jdbcInsert.execute(args);
 
-        return rowsAffected.intValue() >0;
+        return rowsAffected.intValue() > 0;
     }
 
     @Override
     public boolean update(int id, Announcement announcement) {
         return jdbcTemplate.update("UPDATE announcements " +
-                                        "SET teacherId = ?," +
-                                        "subjectId = ?," +
-                                        "date = ?," +
-                                        "title = ?," +
-                                        "content = ?," +
-                                        "WHERE announcementId = ?", new Object[]{announcement.getTeacherId(), announcement.getSubjectId(), announcement.getDate(), announcement.getTitle(), announcement.getContent(), id}) == 1;
+                "SET teacherId = ?," +
+                "courseId = ?," +
+                "date = ?," +
+                "title = ?," +
+                "content = ?" +
+                "WHERE announcementId = ?", new Object[]{announcement.getTeacherId(), announcement.getcourseId(), announcement.getDate(), announcement.getTitle(), announcement.getContent(), id}) == 1;
     }
 
     @Override
@@ -63,11 +64,11 @@ public class AnnouncementDaoImpl implements AnnouncementDao {
 
     @Override
     public List<Announcement> listByCourse(int courseId) {
-        return new ArrayList<>(jdbcTemplate.query("SELECT * FROM announcements WHERE subjectId = ?",new Object[]{courseId}, ROW_MAPPER));
+        return new ArrayList<>(jdbcTemplate.query("SELECT * FROM announcements WHERE courseId = ?", new Object[]{courseId}, ROW_MAPPER));
     }
 
     @Override
     public Optional<Announcement> getById(int id) {
-        return jdbcTemplate.query("SELECT * FROM announcements WHERE announcementId = ?",new Object[]{id},ROW_MAPPER).stream().findFirst();
+        return jdbcTemplate.query("SELECT * FROM announcements WHERE announcementId = ?", new Object[]{id}, ROW_MAPPER).stream().findFirst();
     }
 }
