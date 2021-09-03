682819669b373877d7636de35374d479a8ca133e
tomas <tcerdeira@itba.edu.ar>
2021-09-03 10:54:15 -0300

    TimeTableDao and Implementation. Table "timetables" that relate Courses to a given day of the week (1=Monday, 2=Tuesday,...), start time (in millisenconds) and duration (in milliseconds)

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/TimeTableDaoImpl.java
@@ -0,0 +1,78 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.interfaces.TimeTableDao;
+import ar.edu.itba.paw.models.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class TimeTableDaoImpl implements TimeTableDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert jdbcInsert;
+    //private static final RowMapper<Teacher> ROW_MAPPER;
+
+    @Autowired
+    public TimeTableDaoImpl(final DataSource ds){
+        jdbcTemplate = new JdbcTemplate(ds);
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("timetables");
+    }
+
+    @Override
+    public boolean create(Course course, int dayOfWeek, long start, long duration) {
+        if (!isValidTimeAndDay(dayOfWeek,start)){
+            return false;
+        }
+        final Map<String,Object> args = new HashMap<>();
+        args.put("course_id", course.getSubjectId());
+        args.put("day_of_week", dayOfWeek);
+        args.put("beginning", start);
+        args.put("duration", duration);
+
+        final Number rowsAffected = jdbcInsert.execute(args);
+        return  rowsAffected.intValue() > 0;
+    }
+
+    @Override
+    public boolean update(int course_id, int dayOfWeek, long start, long duration) {
+        if (!isValidTimeAndDay(dayOfWeek,start)){
+            return false;
+        }
+        return jdbcTemplate.update("UPDATE timetables " +
+                "SET day_of_week = ?," +
+                "beginning = ?," +
+                "duration = ?," +
+                "WHERE course_id = ?", new Object[]{dayOfWeek,start,duration,course_id}) == 1;
+    }
+
+    @Override
+    public boolean delete(int course_id) {
+        return jdbcTemplate.update("DELETE FROM timetables WHERE course_id = ?", new Object[]{course_id}) == 1;
+    }
+
+    @Override
+    public Optional<Integer> getDayOfWeekOfCourseById(long course_id) {
+        return Optional.of(jdbcTemplate.queryForObject("SELECT day_of_week FROM timetables WHERE course_id = ?",new Object[]{course_id}, Integer.class));
+    }
+
+    @Override
+    public Optional<Long> getStartOfCourseById(long course_id) {
+        return Optional.of(jdbcTemplate.queryForObject("SELECT beginning FROM timetables WHERE course_id = ?",new Object[]{course_id}, Long.class));
+    }
+
+    @Override
+    public Optional<Long> getDurationOfCourseById(long course_id) {
+        return Optional.of(jdbcTemplate.queryForObject("SELECT duration FROM timetables WHERE course_id = ?",new Object[]{course_id}, Long.class));
+    }
+
+    private boolean isValidTimeAndDay(int dayOfWeek, long start){
+        return dayOfWeek <= 7 && dayOfWeek >= 1 && start < 86400000;
+    }
+}
--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -14,11 +14,25 @@ CREATE TABLE IF NOT EXISTS  courses (
                 board varchar(50),
                 year INTEGER );
 
-        CREATE TABLE IF NOT EXISTS  announcements (
-                announcementId INTEGER,
-                teacherId INTEGER,
-                subjectId INTEGER, 
-                title varchar (50), 
-                content varchar, 
-                date DATE, 
-                PRIMARY KEY(announcementId, teacherId, subjectId) );
+CREATE TABLE IF NOT EXISTS  announcements (
+               announcementId INTEGER,
+               teacherId INTEGER,
+               subjectId INTEGER,
+               title varchar (50),
+               content varchar,
+               date DATE,
+               PRIMARY KEY(announcementId, teacherId, subjectId) );
+
+CREATE TABLE IF NOT EXISTS  students (
+                id INTEGER PRIMARY KEY,
+                name varchar(50),
+                surname varchar (50),
+                email varchar (50),
+                username varchar(50),
+                password varchar (50) );
+
+CREATE TABLE IF NOT EXISTS  timetables (
+                course_id INTEGER FOREIGN KEY,
+                day_of_week INTEGER,
+                beginning INTEGER,
+                duration INTEGER );
