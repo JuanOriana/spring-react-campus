e3b432ddaa99f6062ee94218c4d7a41d41d1f49f
Reversive <matiaspavan@gmail.com>
2021-09-04 23:59:56 -0300

    Replaced Teacher/Student models to a single User model

--- models/src/main/java/ar/edu/itba/paw/models/User.java
+++ models/src/main/java/ar/edu/itba/paw/models/User.java
@@ -1,38 +1,91 @@
 package ar.edu.itba.paw.models;
 
 public class User {
-    private String id, name, password;
+    private String userId, fileNumber, name, surname, username, email, password;
+    private boolean isAdmin;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
+    public User(String userId, String fileNumber, String name, String surname, String username, String email, String password, boolean isAdmin) {
+        this.userId = userId;
+        this.fileNumber = fileNumber;
         this.name = name;
-    }
-
-    public void setPassword(String password) {
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
-    public User() {
-    }
-
-    public User(String id, String name, String password) {
-        this.id = id;
+    public User(String fileNumber, String name, String surname, String username, String email, String password, boolean isAdmin) {
+        this.fileNumber = fileNumber;
         this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.email = email;
         this.password = password;
+        this.isAdmin = isAdmin;
     }
 
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFileNumber() {
+        return fileNumber;
+    }
+
+    public void setFileNumber(String fileNumber) {
+        this.fileNumber = fileNumber;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
     }
 }
