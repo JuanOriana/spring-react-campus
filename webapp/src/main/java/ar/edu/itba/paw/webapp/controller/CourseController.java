7bc675bcc1da3947ccae3ef46e235a228ca18c62
Reversive <matiaspavan@gmail.com>
2021-09-06 14:24:33 -0300

    Added proper error handling by throwing an exception when the course does not exist

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
@@ -6,12 +6,16 @@ import ar.edu.itba.paw.models.Announcement;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.Role;
 import ar.edu.itba.paw.models.User;
+import ar.edu.itba.paw.webapp.exception.CourseNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -30,21 +34,21 @@ public class CourseController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CourseController.class);
 
+    @ExceptionHandler(CourseNotFoundException.class)
+    @ResponseStatus(code = HttpStatus.NOT_FOUND)
+    public ModelAndView noSuchCourse() {
+        ModelAndView mav = new ModelAndView("errorPage");
+        mav.addObject("errorMsg", "Course does not exist");
+        return mav;
+    }
+
     @RequestMapping("/{courseId}")
     public ModelAndView announcements(@PathVariable int courseId) {
-        final ModelAndView mav;
+        final ModelAndView mav = new ModelAndView("course");
         List<Announcement> announcements = announcementService.listByCourse(courseId);
-
         // Add proper handling in the future, need to check if user has permission to access this course
-        Optional<Course> course = courseService.getById(courseId);
-        if(course.isPresent()) {
-            mav = new ModelAndView("course");
-            mav.addObject("course", course.get());
-            mav.addObject("announcementList", announcements);
-        } else {
-            mav = new ModelAndView("errorPage");
-            mav.addObject("errorMsg", "Course does not exist");
-        }
+        mav.addObject("course", courseService.getById(courseId).orElseThrow(CourseNotFoundException::new));
+        mav.addObject("announcementList", announcements);
         return mav;
     }
 
---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exception/CourseNotFoundException.java
@@ -0,0 +1,24 @@
+package ar.edu.itba.paw.webapp.exception;
+
+import java.util.function.Supplier;
+
+public class CourseNotFoundException extends RuntimeException {
+    public CourseNotFoundException() {
+    }
+
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+
+    public CourseNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CourseNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public CourseNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
