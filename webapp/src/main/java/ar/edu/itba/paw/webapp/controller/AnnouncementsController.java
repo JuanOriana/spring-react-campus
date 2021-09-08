a14cf7c0ce203545adb449440f6386e0cc4e871b
Reversive <matiaspavan@gmail.com>
2021-09-07 21:57:34 -0300

    Added page parameter to announcements endpoint

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AnnouncementsController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AnnouncementsController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Comparator;
@@ -21,10 +22,12 @@ public class AnnouncementsController {
     AnnouncementService announcementService;
 
     @RequestMapping("/announcements")
-    public ModelAndView announcements() {
+    public ModelAndView announcements(@RequestParam(value = "page", required = false, defaultValue = "0") Long page) {
         ModelAndView mav = new ModelAndView("announcements");
         List<Announcement> announcements = announcementService.list(orderByDate);
         mav.addObject("announcementList", announcements);
         return mav;
     }
+
+
 }
