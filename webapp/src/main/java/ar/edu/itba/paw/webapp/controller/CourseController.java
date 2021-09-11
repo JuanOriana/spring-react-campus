3f847170e717b722485bfa9e269359e8f03eded3
Juan <joriana@itba.edu.ar>
2021-09-11 15:44:55 -0300

    Files page accesible from navbar

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/CourseController.java
@@ -3,7 +3,6 @@ package ar.edu.itba.paw.webapp.controller;
 import ar.edu.itba.paw.interfaces.AnnouncementService;
 import ar.edu.itba.paw.interfaces.CourseService;
 import ar.edu.itba.paw.models.Announcement;
-import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.Role;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.exception.CourseNotFoundException;
@@ -73,7 +72,7 @@ public class CourseController {
 
     @RequestMapping("/course/{courseId}/files")
     public ModelAndView files(@PathVariable int courseId) {
-        final ModelAndView mav = new ModelAndView("files");
+        final ModelAndView mav = new ModelAndView("course-files");
         mav.addObject("course", courseService.getById(courseId).orElseThrow(CourseNotFoundException::new));
         return mav;
     }
