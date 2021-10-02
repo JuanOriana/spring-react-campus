e2a9c9e7a74f474dda30deb9483e6d33e56b9f64
santigarciam <sangarcia@itba.edu.ar>
2021-10-02 12:06:32 -0300

    Refactored mailingService. Removed unused methods and added new methods to notified when is a new announcement or a new user

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/MailingService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/MailingService.java
@@ -1,40 +1,33 @@
 package ar.edu.itba.paw.interfaces;
 
 
-import java.util.Collections;
+import ar.edu.itba.paw.models.Course;
+
 import java.util.List;
 
 public interface MailingService {
-
     /**
-     * Attempts to send a broadcast email to a list of destintations. Using content type = text/plain
-     * @param to  Set a list to send the email
-     * @param subject The subject of the email
-     * @param content The content of the email
-     */
-    void sendTextPlainBroadcastEmail(List<String> to, String subject, String content);
-
-    /**
-     * Attempts to send a broadcast email to a list of destintations. Using content type = text/html
-     * @param to  Set a list to send the email
-     * @param subject The subject of the email
-     * @param content The content of the email
-     */
-    void sendHtmlBroadcastEmail(List<String> to, String subject, String content);
-
-    /**
-     * Attempts to send a email to "to" destintation. Using content type = text/plain
-     * @param to  Set a list to send the email
+     * Attempts to email "to" destination. Using content type = text/plain
+     *
+     * @param to      Set a list to send the email
      * @param subject The subject of the email
      * @param content The content of the email
      */
     void sendTextPlainEmail(String replyTo, String to, String subject, String content);
 
     /**
-     * Attempts to send a email to "to" destintation. Using content type = text/plain
-     * @param to  Set a list to send the email
-     * @param subject The subject of the email
-     * @param content The content of the email
+     * Attempts to send a broadcast email to every student in course notifying of a new announcement
+     * @param to list of students emails addresses in course
+     * @param title The title of the new announcement
+     * @param content The content of the new announcement
+     * @param course The course where de announcement what published
      */
-    void sendHtmlEmail(String replyTo, String to, String subject, String content);
+    void sendNewAnnouncementNotification(List<String> to,String title,String content, Course course);
+
+    /**
+     * Attempts to email the new enrolled user
+     * @param to The new user email address
+     */
+    void sendNewUserNotification(String to);
+
 }
