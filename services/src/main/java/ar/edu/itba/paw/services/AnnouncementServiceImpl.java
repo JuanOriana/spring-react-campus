5598deecdc52edb96be05dd5b3c82b6f7d9a10f3
santigarciam <sangarcia@itba.edu.ar>
2021-10-02 12:07:13 -0300

    Implemented the new mailingService method in announcementServiceImpl

--- services/src/main/java/ar/edu/itba/paw/services/AnnouncementServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AnnouncementServiceImpl.java
@@ -32,8 +32,7 @@ public class AnnouncementServiceImpl implements AnnouncementService {
         List<User> userList = courseService.getStudents(announcement.getCourse().getCourseId());
         List<String> emailList = new ArrayList<>();
         userList.forEach(u->emailList.add(u.getEmail()));
-        mailingService.sendTextPlainBroadcastEmail(emailList, "Nuevo anuncio en curso " + announcement.getTitle(),
-                announcement.getContent());
+        mailingService.sendNewAnnouncementNotification(emailList, title, content, course);
         return announcement;
     }
 
