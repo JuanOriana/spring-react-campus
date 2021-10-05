e5b6f345ef97b9d36257b836b2b17701e8fda517
santigarciam <sangarcia@itba.edu.ar>
2021-10-05 18:07:13 -0300

    Added new method to send a html email to a teacher and removed method to sendTextPlainEmail

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/MailingService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/MailingService.java
@@ -9,21 +9,23 @@ import java.util.List;
 public interface MailingService {
     /**
      * Attempts to email "to" destination. Using content type = text/plain
-     *
-     * @param to      Set a list to send the email
-     * @param subject The subject of the email
-     * @param content The content of the email
+     * @param student who sends the email
+     * @param to    the destination
+     * @param subject   The email subject
+     * @param content   The email content
+     * @param course    The course where the email is being send from
      */
-    void sendTextPlainEmail(String replyTo, String to, String subject, String content);
+    void sendTeacherEmail(User student, String to, String subject, String content, Course course);
 
     /**
      * Attempts to send a broadcast email to every student in course notifying of a new announcement
-     * @param to list of students emails addresses in course
-     * @param title The title of the new announcement
+     *
+     * @param to      list of students emails addresses in course
+     * @param title   The title of the new announcement
      * @param content The content of the new announcement
-     * @param course The course where de announcement what published
-     * @param author The author of the announcement
+     * @param course  The course where de announcement what published
+     * @param author  The author of the announcement
      */
-    void sendNewAnnouncementNotification(List<String> to,String title,String content, Course course, User author);
+    void sendNewAnnouncementNotification(List<String> to, String title, String content, Course course, User author);
 
 }
