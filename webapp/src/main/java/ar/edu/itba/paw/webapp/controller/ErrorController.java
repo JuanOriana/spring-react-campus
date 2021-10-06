4fb493c376dfb00e87451b5c44c0cbd50f533e05
santigarciam <sangarcia@itba.edu.ar>
2021-10-06 17:13:08 -0300

    Added 400 error page and changed announcement form messages

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -7,6 +7,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ErrorController {
 
+    @GetMapping(value = "400")
+    public ModelAndView serverError4() {
+        return new ModelAndView("400");
+    }
+
     @GetMapping(value = "403")
     public ModelAndView accessDenied() {
         return new ModelAndView("403");
@@ -16,4 +21,10 @@ public class ErrorController {
     public ModelAndView noSuchPath() {
         return new ModelAndView("404");
     }
+
+    @GetMapping(value = "500")
+    public ModelAndView serverError() {
+        return new ModelAndView("500");
+    }
+
 }
