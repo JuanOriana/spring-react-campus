4b5b298266074eed41a739716b808748d2fb7c43
tomas <tcerdeira@itba.edu.ar>
2021-10-19 09:04:08 -0300

    Timetable model migrated to JPA.

--- models/src/main/java/ar/edu/itba/paw/models/Timetable.java
+++ models/src/main/java/ar/edu/itba/paw/models/Timetable.java
@@ -1,15 +1,31 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
 import java.sql.Time;
 
+@Entity
+@Table(name = "timetables")
 public class Timetable {
-    private Long courseId;
+
+    @ManyToOne
+    @JoinColumn(name = "courseId")
+    private Course course;
+
+    @Column
     private Integer dayOfWeek;
+
+    @Column
     private Time startTime;
+
+    @Column
     private Time endTime;
 
-    public Timetable(Long courseId, int dayOfWeek, Time startTime, Time endTime) {
-        this.courseId = courseId;
+    /* Default */ Timetable() {
+        // Just for Hibernate
+    }
+
+    public Timetable(Course course, int dayOfWeek, Time startTime, Time endTime) {
+        this.course = course;
         this.dayOfWeek = dayOfWeek;
         this.startTime = startTime;
         this.endTime = endTime;
@@ -21,12 +37,12 @@ public class Timetable {
         this.endTime = endTime;
     }
 
-    public Long getCourseId() {
-        return courseId;
+    public Course getCourse() {
+        return course;
     }
 
-    public void setCourseId(Long courseId) {
-        this.courseId = courseId;
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     public Integer getDayOfWeek() {
