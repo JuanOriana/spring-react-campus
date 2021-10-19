730b01a66fcd9aa5dd76242c70d1d39270956d76
tomas <tcerdeira@itba.edu.ar>
2021-10-19 09:39:05 -0300

    Initial implementation of TimetableDaoJpa

--- persistence/src/main/java/ar/edu/itba/paw/persistence/TimetableDaoJpa.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/TimetableDaoJpa.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.sql.Time;
 import java.util.List;
+import java.util.Optional;
 
 @Primary
 @Repository
@@ -20,21 +22,34 @@ public class TimetableDaoJpa implements TimetableDao {
 
     @Override
     public boolean create(Course course, int dayOfWeek, Time start, Time end) {
-        return false;
+        final Timetable timetable = new Timetable(course, dayOfWeek, start, end);
+        em.persist(timetable);
+        return true;
     }
 
     @Override
     public boolean update(Long courseId, int dayOfWeek, Time start, Time end) {
-        return false;
+        List<Timetable> dbTimetable = findById(courseId);
+        if (dbTimetable.size() == 0) return false;
+        for (Timetable t : dbTimetable){
+            t.setDayOfWeek(dayOfWeek);
+            t.setBegins(start);
+            t.setEnd(end);
+        }
+        em.flush();
+        return true;
     }
 
     @Override
     public boolean delete(Long courseId) {
-        return false;
+        int deletedCount = em.createQuery("DELETE FROM Timetable t WHERE t.course = :courseId").setParameter("courseId", courseId).executeUpdate();
+        return deletedCount > 0;
     }
 
     @Override
     public List<Timetable> findById(Long courseId) {
-        return null;
+        final TypedQuery<Timetable> query = em.createQuery("SELECT t FROM Timetable t WHERE t.course = :courseId", Timetable.class);
+        query.setParameter("courseId", courseId);
+        return query.getResultList();
     }
 }
