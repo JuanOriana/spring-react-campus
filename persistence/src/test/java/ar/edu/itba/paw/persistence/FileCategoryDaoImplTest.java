ddbeb028af4928a5e6e2e3c1f136d3f54d51699c
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-10-31 23:15:26 -0300

    Added file category jpa tests

--- persistence/src/test/java/ar/edu/itba/paw/persistence/FileCategoryDaoImplTest.java
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/FileCategoryDaoImplTest.java
@@ -2,73 +2,68 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.interfaces.FileCategoryDao;
 import ar.edu.itba.paw.models.FileCategory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
-@Sql("classpath:schema.sql")
 @Rollback
 @Transactional
-public class FileCategoryDaoImplTest extends BasicPopulator {
+public class FileCategoryDaoImplTest {
+
+    private static final String FILE_CATEGORY = "exam";
+    private static final long FILE_CATEGORY_ID = 1L;
 
     @Autowired
     private FileCategoryDao fileCategoryDao;
 
-    private static final RowMapper<FileCategory> FILE_CATEGORY_ROW_MAPPER = (rs, rowNum) -> new FileCategory(rs.getLong("categoryId"), rs.getString("categoryName"));
-
-    @Before
-    public void setUp() {
-        super.setUp();
-        insertFileCategory(FILE_CATEGORY_ID, FILE_CATEGORY);
-    }
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     public void testCreate() {
         FileCategory fileCategory = fileCategoryDao.create(FILE_CATEGORY);
         assertNotNull(fileCategory);
-        assertEquals(2, JdbcTestUtils.countRowsInTable(jdbcTemplate, "file_categories"));
     }
 
     @Test
     public void testUpdate() {
+        FileCategory fileCategory = fileCategoryDao.create(FILE_CATEGORY);
         String newCategoryName = "Guia Practica";
-        boolean wasUpdated = fileCategoryDao.update(FILE_CATEGORY_ID, newCategoryName);
+        boolean wasUpdated = fileCategoryDao.update(fileCategory.getCategoryId(), newCategoryName);
         assertTrue(wasUpdated);
-
-        FileCategory fileCategoryFromDB = jdbcTemplate.query("SELECT * FROM file_categories WHERE categoryId = ?;", new Object[]{FILE_CATEGORY_ID}, FILE_CATEGORY_ROW_MAPPER).get(0);
-
-        assertEquals(FILE_CATEGORY_ID.longValue(), fileCategoryFromDB.getCategoryId());
-        assertEquals(newCategoryName, fileCategoryFromDB.getCategoryName());
+        Optional<FileCategory> fileCategoryUpdated = fileCategoryDao.findById(fileCategory.getCategoryId());
+        assertTrue(fileCategoryUpdated.isPresent());
+        assertEquals(fileCategory.getCategoryId(), fileCategoryUpdated.get().getCategoryId());
+        assertEquals(newCategoryName, fileCategoryUpdated.get().getCategoryName());
     }
 
     @Test
     public void testDelete() {
-        boolean wasDeleted = fileCategoryDao.delete(FILE_CATEGORY_ID);
+        FileCategory fileCategory = fileCategoryDao.create(FILE_CATEGORY);
+        boolean wasDeleted = fileCategoryDao.delete(fileCategory.getCategoryId());
         assertTrue(wasDeleted);
-        assertEquals(0, JdbcTestUtils.countRowsInTable(jdbcTemplate, "file_categories"));
     }
 
     @Test
     public void testGetCategories() {
+        fileCategoryDao.create(FILE_CATEGORY);
         List<FileCategory> list = fileCategoryDao.getCategories();
         assertEquals(1, list.size());
-
         FileCategory fileCategory = list.get(0);
-        assertEquals(FILE_CATEGORY_ID.longValue(), fileCategory.getCategoryId());
+        assertEquals(FILE_CATEGORY_ID, fileCategory.getCategoryId());
 
     }
 
--- services/src/main/java/ar/edu/itba/paw/services/AnswerServiceImpl.java
+++ services/src/main/java/ar/edu/itba/paw/services/AnswerServiceImpl.java
@@ -32,7 +32,7 @@ public class AnswerServiceImpl implements AnswerService {
     public Answer create(Long examId, Long studentId, String answerFileName, byte[] answerFile, Long answerFileSize, LocalDateTime deliveredTime) {
         Exam exam = examDao.findById(examId).orElseThrow(ExamNotFoundException::new);
         FileModel answerFileModel = fileDao.create(answerFileSize, deliveredTime, answerFileName, answerFile, exam.getCourse());
-        answerFileModel.setHidden(true); //Para que no aparezca en como material de la materia
+        answerFileModel.setHidden(true);
         long examCategoryId = 0;
         for (FileCategory fc : fileCategoryDao.getCategories()) {
             if (fc.getCategoryName().equalsIgnoreCase("exam")) {
@@ -50,11 +50,12 @@ public class AnswerServiceImpl implements AnswerService {
         return answersDao.update(answerId, answer);
     }
 
+    @Transactional
     @Override
     public Answer updateEmptyAnswer(Long examId, User student, String answerFileName, byte[] answerFile, Long answerFileSize, LocalDateTime deliveredTime){
         Exam exam = examDao.findById(examId).orElseThrow(ExamNotFoundException::new);
         FileModel answerFileModel = fileDao.create(answerFileSize, deliveredTime, answerFileName, answerFile, exam.getCourse());
-        answerFileModel.setHidden(true); //Para que no aparezca en como material de la materia
+        answerFileModel.setHidden(true);
         long examCategoryId = 0;
         for (FileCategory fc : fileCategoryDao.getCategories()) {
             if (fc.getCategoryName().equalsIgnoreCase("exam")) {
