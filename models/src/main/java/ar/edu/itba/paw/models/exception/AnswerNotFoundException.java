2faf316d5cbdd10b1a0ef820784b499c2ca07b46
Matias Enrique Pavan <matiaspavan@gmail.com>
2021-11-01 10:41:23 -0300

    Refactored undoExamCorrection method with jpa query

---
+++ models/src/main/java/ar/edu/itba/paw/models/exception/AnswerNotFoundException.java
@@ -0,0 +1,22 @@
+package ar.edu.itba.paw.models.exception;
+
+public class AnswerNotFoundException extends RuntimeException {
+    public AnswerNotFoundException() {
+    }
+
+    public AnswerNotFoundException(String message) {
+        super(message);
+    }
+
+    public AnswerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AnswerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public AnswerNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
