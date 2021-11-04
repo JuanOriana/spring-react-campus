b16dd3aab7becfeba12f490d011c0e1aec9bf4e1
santigarciam <sangarcia@itba.edu.ar>
2021-11-04 19:00:33 -0300

    Created and impl examJpa tests

---
+++ persistence/src/test/java/ar/edu/itba/paw/persistence/ExamDaoJpaTest.java
@@ -0,0 +1,84 @@
+package ar.edu.itba.paw.persistence;
+
+
+import ar.edu.itba.paw.interfaces.ExamDao;
+import ar.edu.itba.paw.models.Course;
+import ar.edu.itba.paw.models.Exam;
+import ar.edu.itba.paw.models.FileModel;
+import ar.edu.itba.paw.models.Subject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:populators/exam_populator.sql")
+@Rollback
+@Transactional
+public class ExamDaoJpaTest extends BasicPopulator {
+    @Autowired
+    private ExamDao examDao;
+
+    private final String EXAM_TITLE = "Exam title";
+    private final String EXAM_DESCRIPTION = "Exam description";
+    private final Long EXAM_ID = 1L;
+
+
+    @Test
+    public void testCreate() {
+        Long fileAnswerId = 9999L;
+        FileModel examFile = createFileModelObject("src/test/resources/test.png", FILE_ID);
+        FileModel answerFile = createFileModelObject("src/test/resources/test.png", fileAnswerId);
+        LocalDateTime startTime = LocalDateTime.MIN;
+        LocalDateTime endTime = LocalDateTime.now();
+        Exam exam = examDao.create(COURSE_ID, EXAM_TITLE, EXAM_DESCRIPTION, examFile, answerFile, startTime, endTime);
+        assertNotNull(exam);
+    }
+
+    @Test
+    public void testDelete() {
+        assertTrue(examDao.delete(EXAM_ID));
+    }
+
+    @Test
+    public void testUpdate() {
+        FileModel examFile = createFileModelObject("src/test/resources/test.png", FILE_ID);
+        Exam exam = new Exam.Builder()
+                .withCourse(new Course(COURSE_YEAR, COURSE_QUARTER, COURSE_BOARD, new Subject(SUBJECT_CODE, SUBJECT_NAME)))
+                .withStartTime(LocalDateTime.MIN)
+                .withEndTime(LocalDateTime.now())
+                .withTitle(EXAM_TITLE)
+                .withDescription(EXAM_DESCRIPTION)
+                .withExamFile(examFile)
+                .build();
+        assertTrue(examDao.update(EXAM_ID,exam));
+    }
+
+    @Test
+    public void testListByCourse() {
+        List<Exam> list = examDao.listByCourse(COURSE_ID);
+
+        assertNotNull(list);
+        assertEquals(1, list.size());
+        assertEquals(EXAM_ID, list.get(0).getExamId());
+    }
+
+    @Test
+    public void testFindById() {
+        final Optional<Exam> examOptional = examDao.findById(EXAM_ID);
+        assertNotNull(examOptional);
+        assertTrue(examOptional.isPresent());
+        assertEquals(EXAM_ID, examOptional.get().getExamId());
+    }
+}
---
+++ persistence/src/test/resources/populators/exam_populator.sql
@@ -0,0 +1,38 @@
+INSERT INTO subjects(subjectid, code, subjectname)
+VALUES (1, 'A22', 'Star Wars');
+
+INSERT INTO courses(courseid, subjectid, quarter, board, year)
+VALUES (1, 1, 2, 'S', 2021);
+
+INSERT INTO users(userid, filenumber, name, surname, username, email, password, isadmin)
+VALUES (1337, 1, 'Santiago', 'Garcia', 'sangarcia', 'sangarcia@itba.edu.ar', 'top_secret', true);
+
+INSERT INTO roles(roleid, rolename)
+VALUES (2, 'Teacher');
+
+INSERT INTO user_to_course(courseid, userid, roleid)
+VALUES (1, 1337, 2);
+
+INSERT INTO file_categories(categoryid, categoryname)
+VALUES (2, 'exam');
+
+INSERT INTO file_extensions(fileextensionid, fileextension)
+VALUES (2, 'other');
+
+INSERT INTO file_extensions(fileextensionid, fileextension)
+VALUES (1, 'pdf');
+
+INSERT INTO files(fileid, filesize, filename, filedate, file, fileextensionid, courseid, downloads, hidden)
+VALUES (1337, 1000, 'test.png', '2021-09-26 21:14:35.138', null, 1, 1, 1, false);
+
+INSERT INTO files(fileid, filesize, filename, filedate, file, fileextensionid, courseid, downloads, hidden)
+VALUES (1338, 1000, 'test2.png', '2021-09-26 21:14:35.138', null, 1, 1, 1, false);
+
+INSERT INTO category_file_relationship(categoryid, fileid)
+VALUES (2, 1337);
+
+INSERT INTO category_file_relationship(categoryid, fileid)
+VALUES (2, 1338);
+--
+INSERT INTO exams(examId,courseId,startTime,endTime,title,description)
+VALUES (1,1,'2021-09-26 21:14:35.138','2021-09-26 22:14:35.138','Exam title','Exam description');
