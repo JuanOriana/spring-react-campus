cb06021a07bbd690e37d282e96ac54540bced0ad
tomas <tcerdeira@itba.edu.ar>
2022-01-12 19:11:17 +0100

    Revert "- Added dependencies for jersey.media.multipart - Initial file upload endpoint implementation"
    This reverts commit 8cfa2643

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/JerseyConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/JerseyConfig.java
@@ -2,13 +2,11 @@ package ar.edu.itba.paw.webapp.config;
 
 import ar.edu.itba.paw.webapp.mappers.ApiExceptionMapper;
 import ar.edu.itba.paw.webapp.mappers.ServerErrorMapper;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         register(ApiExceptionMapper.class);
         register(ServerErrorMapper.class);
-        register(MultiPartFeature.class);
     }
 }
