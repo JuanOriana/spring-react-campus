98b1734489eb6338802d6ced808375abaf1bb466
tomas <tcerdeira@itba.edu.ar>
2022-01-14 21:33:24 +0100

    - Added newCourse end point to AdminControllerREST for creating new courses --- CourseFormDto with its attributes constraints

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
@@ -4,14 +4,21 @@ import ar.edu.itba.paw.interfaces.CourseService;
 import ar.edu.itba.paw.interfaces.RoleService;
 import ar.edu.itba.paw.interfaces.SubjectService;
 import ar.edu.itba.paw.interfaces.UserService;
+import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.constraint.validator.DtoConstraintValidator;
+import ar.edu.itba.paw.webapp.dto.CourseFormDto;
 import ar.edu.itba.paw.webapp.dto.UserRegisterFormDto;
+import ar.edu.itba.paw.webapp.form.CourseForm;
 import ar.edu.itba.paw.webapp.security.api.exception.DtoValidationException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -43,6 +50,7 @@ public class AdminControllerREST {
 
 
     @POST
+    @Path("/user/new")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(value = {MediaType.APPLICATION_JSON, })
     public Response newUser(@Valid UserRegisterFormDto userRegisterForm) throws DtoValidationException {
@@ -54,6 +62,22 @@ public class AdminControllerREST {
                     userRegisterForm.getPassword(), false);
             LOGGER.debug("User of name {} created", user.getUsername());
             return Response.ok(new GenericEntity<User>(user) {
+            }).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    @POST
+    @Path("/course/new")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(value = {MediaType.APPLICATION_JSON, })
+    public Response newCourse(@Valid CourseFormDto courseForm) throws DtoValidationException { //TODO: ver bien como seria la parte del json para startTimes y endTimes
+        if(courseForm != null) {
+            Course course = courseService.create(courseForm.getYear(), courseForm.getQuarter(), courseForm.getBoard()
+                    , courseForm.getSubjectId(), courseForm.getStartTimes(), courseForm.getEndTimes());
+            LOGGER.debug("Created course in year {} in quarter {} of subjectId {} with id {}", courseForm.getYear(),
+                    courseForm.getBoard(), courseForm.getSubjectId(), course.getCourseId());
+            return Response.ok(new GenericEntity<Course>(course) {
             }).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
