5af0346cbe47e44124225dd07f9b7597da3fd0f0
tomas <tcerdeira@itba.edu.ar>
2022-01-15 19:05:31 +0100

    - Initial implementation of addUserToCourse endpoint

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
@@ -10,11 +10,18 @@ import ar.edu.itba.paw.webapp.constraint.validator.DtoConstraintValidator;
 import ar.edu.itba.paw.webapp.dto.CourseFormDto;
 import ar.edu.itba.paw.webapp.dto.NextFileNumberDto;
 import ar.edu.itba.paw.webapp.dto.UserRegisterFormDto;
+import ar.edu.itba.paw.webapp.dto.UserToCourseFormDto;
+import ar.edu.itba.paw.webapp.form.UserToCourseForm;
 import ar.edu.itba.paw.webapp.security.api.exception.DtoValidationException;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -66,7 +73,7 @@ public class AdminControllerREST {
                     userRegisterForm.getPassword(), false);
             LOGGER.debug("User of name {} created", user.getUsername());
             return Response.ok(new GenericEntity<User>(user) {
-            }).build();
+            }).status(Response.Status.CREATED).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
@@ -82,7 +89,20 @@ public class AdminControllerREST {
             LOGGER.debug("Created course in year {} in quarter {} of subjectId {} with id {}", courseForm.getYear(),
                     courseForm.getBoard(), courseForm.getSubjectId(), course.getCourseId());
             return Response.ok(new GenericEntity<Course>(course) {
-            }).build();
+            }).status(Response.Status.CREATED).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    @POST
+    @Path("/course/enroll")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(value = {MediaType.APPLICATION_JSON, })
+    public Response addUserToCourse(@Valid UserToCourseFormDto userToCourseForm, @RequestParam(name = "courseId") Long courseId) throws DtoValidationException{
+        if (userToCourseForm != null){
+            courseService.enroll(userToCourseForm.getUserId(), courseId, userToCourseForm.getRoleId());
+            LOGGER.debug("User {} successfully enrolled in {}", userToCourseForm.getUserId(), courseId);
+            return Response.ok().build(); //TODO: que deberia devolver?
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
