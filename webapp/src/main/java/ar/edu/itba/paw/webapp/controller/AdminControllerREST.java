6ef1200b868735a47288bd8d39387bbc4d22f03e
tomas <tcerdeira@itba.edu.ar>
2022-01-17 16:36:20 +0100

    - Made endpoint /admin/course/new return 303 "See other" status code for pointing to location /admin/course/enroll?courseId=x - Started /course/all endpoint

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminControllerREST.java
@@ -4,6 +4,7 @@ import ar.edu.itba.paw.interfaces.CourseService;
 import ar.edu.itba.paw.interfaces.RoleService;
 import ar.edu.itba.paw.interfaces.SubjectService;
 import ar.edu.itba.paw.interfaces.UserService;
+import ar.edu.itba.paw.models.CampusPage;
 import ar.edu.itba.paw.models.Course;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.webapp.constraint.validator.DtoConstraintValidator;
@@ -27,11 +28,16 @@ import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Calendar;
 
 @Path("admin")
 @Component
 @ComponentScan({"ar.edu.itba.paw.webapp.constraint.validator"}) //TODO: ver si esto se puede sacar, se lo puse por que sino no me encontraba el bean DtoConstraintValidator
 public class AdminControllerREST {
+
+    @Context
+    private UriInfo uriInfo;
 
     @Autowired
     private DtoConstraintValidator DtoValidator;
@@ -81,28 +87,56 @@ public class AdminControllerREST {
     @Path("/course/new")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(value = {MediaType.APPLICATION_JSON, })
-    public Response newCourse(@Valid CourseFormDto courseForm) throws DtoValidationException { //TODO: ver bien como seria la parte del json para startTimes y endTimes
+    public Response newCourse(@Valid CourseFormDto courseForm) throws DtoValidationException, URISyntaxException { //TODO: ver bien como seria la parte del json para startTimes y endTimes
         if(courseForm != null) {
             Course course = courseService.create(courseForm.getYear(), courseForm.getQuarter(), courseForm.getBoard()
                     , courseForm.getSubjectId(), courseForm.getStartTimes(), courseForm.getEndTimes());
             LOGGER.debug("Created course in year {} in quarter {} of subjectId {} with id {}", courseForm.getYear(),
                     courseForm.getBoard(), courseForm.getSubjectId(), course.getCourseId());
-            return Response.ok(new GenericEntity<Course>(course) {}).status(Response.Status.CREATED).build();
+            URI enrollUsers = new URI(uriInfo.getBaseUri().normalize().toString() + "admin/course/enroll?courseId=" + course.getCourseId());
+            return Response.seeOther(enrollUsers).status(Response.Status.CREATED).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
     @POST
-    @Path("/course/enroll/{courseId}")
+    @Path("/course/enroll")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(value = {MediaType.APPLICATION_JSON, })
-    public Response addUserToCourse(@Valid UserToCourseFormDto userToCourseForm, @PathParam("courseId") Long courseId) throws DtoValidationException{
+    public Response addUserToCourse(@Valid UserToCourseFormDto userToCourseForm, @QueryParam("courseId") Long courseId) throws DtoValidationException{
         if (userToCourseForm != null){
             courseService.enroll(userToCourseForm.getUserId(), courseId, userToCourseForm.getRoleId());
             LOGGER.debug("User {} successfully enrolled in {}", userToCourseForm.getUserId(), courseId);
-            return Response.ok().build(); //TODO: que deberia devolver?
+            return Response.ok().status(Response.Status.ACCEPTED).build(); //TODO: devolver algo mas?
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
+
+//    @GET
+//    @Path("/course/all")
+//    @Produces(value = {MediaType.APPLICATION_JSON, })
+//    public Response allCourses(@QueryParam("year") Integer year, @QueryParam("quarter") Integer quarter) { //TODO: falta agregar paginacion
+//        if (year == null){
+//            year = Calendar.getInstance().get(Calendar.YEAR);
+//        }
+//        if (quarter == null){
+//            if (Calendar.getInstance().get(Calendar.MONTH) <= 6){
+//                quarter = 1;
+//            }
+//            else{
+//                quarter = 2;
+//            }
+//        }
+//
+////        CampusPage<Course> courses = courseService.listByYearQuarter(year,quarter, page, pageSize);
+////        mav.addObject("courses", courses.getContent());
+////        mav.addObject("year",year);
+////        mav.addObject("allYears",courseService.getAvailableYears());
+////        mav.addObject("quarter",quarter);
+////        mav.addObject("currentPage", courses.getPage());
+////        mav.addObject("maxPage", courses.getTotal());
+////        mav.addObject("pageSize", courses.getSize());
+////        return mav;
+//    }
 
 }
