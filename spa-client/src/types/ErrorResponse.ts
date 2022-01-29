b71d80e90b3f4861f6fb83afc07ba9f798747172
santigarciam <sangarcia@itba.edu.ar>
2022-01-29 12:18:45 -0300

    Moved types directory into src/

--- spa-client/src/types/ErrorResponse.ts
+++ spa-client/src/types/ErrorResponse.ts
@@ -2,7 +2,7 @@ export class ErrorResponse{
     private readonly code:number;
     private readonly descritpion:string;
 
-    private constructor(code:number,description:string){
+    public constructor(code:number,description:string){
         this.code = code;
         this.descritpion = description;
     }
