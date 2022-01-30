6b505735a70b985d0def7d130491197416ec19a0
Matias Enrique Pavan <matiaspavan@gmail.com>
2022-01-30 11:39:57 -0300

    Answer controller overall refactor, letting the mapper handle errors

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AnswerController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AnswerController.java
@@ -8,9 +8,7 @@ import ar.edu.itba.paw.models.exception.AnswerNotFoundException;
 import ar.edu.itba.paw.webapp.dto.AnswerDto;
 import ar.edu.itba.paw.webapp.security.service.AuthFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
-
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -19,7 +17,6 @@ import javax.ws.rs.core.UriInfo;
 
 @Path("answers")
 @Component
-@ComponentScan({"ar.edu.itba.paw.webapp.constraint.validator"})
 public class AnswerController {
 
     @Context
@@ -34,36 +31,31 @@ public class AnswerController {
     @Autowired
     private AuthFacade authFacade;
 
-
     @GET
     @Path("{answerId}")
-    @Produces(value={MediaType.APPLICATION_JSON})
-    public Response getAnswer(@PathParam("answerId")Long answerId){
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public Response getAnswer(@PathParam("answerId") Long answerId) {
         Answer answer = answerService.findById(answerId).orElseThrow(AnswerNotFoundException::new);
         Long userId = authFacade.getCurrentUserId();
-
-        if(answer.getStudent().getUserId().equals(userId) || courseService.isPrivileged(userId, answer.getExam().getCourse().getCourseId())){
-            return Response.ok(AnswerDto.fromAnswer(uriInfo,answer)).build();
+        if(!answer.getStudent().getUserId().equals(userId) && !courseService.isPrivileged(userId, answer.getExam().getCourse().getCourseId())) {
+            throw new ForbiddenException();
         }
-
-        return Response.status(Response.Status.FORBIDDEN).build();
+        return Response.ok(AnswerDto.fromAnswer(uriInfo,answer)).build();
     }
 
 
     @DELETE
     @Path("{answerId}")
-    @Produces(value={MediaType.APPLICATION_JSON})
-    public Response deleteAnswer(@PathParam("answerId")Long answerId){
+    @Produces(value= MediaType.APPLICATION_JSON)
+    public Response deleteAnswer(@PathParam("answerId") Long answerId) {
         Answer answer = answerService.findById(answerId).orElseThrow(AnswerNotFoundException::new);
         Long userId = authFacade.getCurrentUserId();
-
-        if(answer.getStudent().getUserId().equals(userId) || courseService.isPrivileged(userId, answer.getExam().getCourse().getCourseId())){
-            if(answerService.delete(answerId)) {
-                return Response.ok().build();
-            }else{
-                return Response.noContent().build();
-            }
+        if(!answer.getStudent().getUserId().equals(userId) && !courseService.isPrivileged(userId, answer.getExam().getCourse().getCourseId())) {
+            throw new ForbiddenException();
         }
-        return Response.status(Response.Status.FORBIDDEN).build();
+        if(!answerService.delete(answerId)) {
+            throw new NotFoundException();
+        }
+        return Response.noContent().build();
     }
 }
