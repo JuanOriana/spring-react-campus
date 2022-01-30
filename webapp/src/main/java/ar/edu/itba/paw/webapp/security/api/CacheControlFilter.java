db7d62afb3e00807836b875531145faf0ad4f6b6
Matias Enrique Pavan <matiaspavan@gmail.com>
2022-01-30 12:08:12 -0300

    Added CORS and Cache-Control filters

--- webapp/src/main/java/ar/edu/itba/paw/webapp/security/api/CacheControlFilter.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/security/api/CacheControlFilter.java
@@ -1,4 +1,4 @@
-package ar.edu.itba.paw.webapp.controller;
+package ar.edu.itba.paw.webapp.security.api;
 
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -6,15 +6,15 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
-@Provider
 public class CacheControlFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+        if(request.getMethod().equals("GET")) {
+            response.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+        }
         filterChain.doFilter(request, response);
     }
 
