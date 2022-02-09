cfe4978bb696013188ff09873d3df5470b8bf26d
Juan <joriana@itba.edu.ar>
2022-02-09 16:19:11 -0300

    Timetable finished

--- spa-client/src/services/index.ts
+++ spa-client/src/services/index.ts
@@ -4,6 +4,8 @@ import { LoginService } from "./LoginService";
 import { UserService } from "./UserService";
 import { FileService } from "./FileServices";
 import { SubjectsService } from "./SubjectsService";
+import { ExamsServices } from "./ExamsService";
+import { AnswersService } from "./AnswersService";
 
 const courseService = new CourseService();
 const announcementsService = new AnnouncementsService();
@@ -11,6 +13,8 @@ const loginService = new LoginService();
 const userService = new UserService();
 const fileService = new FileService();
 const subjectsService = new SubjectsService();
+const examsService = new ExamsServices();
+const answersService = new AnswersService();
 
 export {
   courseService,
@@ -19,4 +23,6 @@ export {
   userService,
   fileService,
   subjectsService,
+  examsService,
+  answersService,
 };
