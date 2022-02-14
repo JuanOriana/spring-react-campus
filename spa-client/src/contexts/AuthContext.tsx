50d4cb2444d1d63e6a23709d410d582b037f6b24
Juan <joriana@itba.edu.ar>
2022-02-14 15:33:53 -0300

    Basic rememberMe handling

--- spa-client/src/contexts/AuthContext.tsx
+++ spa-client/src/contexts/AuthContext.tsx
@@ -5,7 +5,11 @@ import { UserModel } from "../types";
 interface AuthContextType {
   user: UserModel | null;
   setUser: (user: UserModel | null) => void;
-  signin: (user: UserModel, callback: VoidFunction) => void;
+  signin: (
+    user: UserModel,
+    rememberMe: boolean,
+    callback: VoidFunction
+  ) => void;
   signout: (callback: VoidFunction) => void;
 }
 
@@ -14,13 +18,18 @@ const AuthContext = React.createContext<AuthContextType>(null!);
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   let [user, setUser] = React.useState<UserModel | null>(null);
 
-  let signin = (newUser: UserModel, callback: VoidFunction) => {
+  let signin = (
+    newUser: UserModel,
+    rememberMe: boolean,
+    callback: VoidFunction
+  ) => {
     return internalAuthProvider.signin(() => {
       setUser(newUser);
       if (!localStorage.getItem("user"))
         localStorage.setItem("user", JSON.stringify(newUser));
       localStorage.setItem("token", newUser.token!);
       localStorage.setItem("isAdmin", newUser.admin ? "true" : "false");
+      localStorage.setItem("rememberMe", rememberMe ? "true" : "false");
       callback();
     });
   };
@@ -31,6 +40,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       localStorage.removeItem("user");
       localStorage.removeItem("token");
       localStorage.removeItem("isAdmin");
+      localStorage.removeItem("rememberMe");
       callback();
     });
   };
